83579779dc80a87e425436d54b36f4e43cfe328f
Adithya V <adithyavinod1943@gmail.com>
2024-07-05 01:12:42 +0530

    Refactor MeetingForm

--- app/(routes)/create-meeting/_components/MeetingForm.jsx
+++ app/(routes)/create-meeting/_components/MeetingForm.jsx
@@ -12,12 +12,16 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import LocationOptions from '@/app/_utils/LocationOptions'
+import Link from 'next/link'
 
 function MeetingForm() {
   const [location,setLocation]=useState()
+  const [eventName,setEventName]=useState('')
+  const [duration,setDuration]=useState(30)
+  const [locationUrl,setLocationUrl]=useState("")
   return (
-    <div className="p-4">
-      <h2 className="flex gap-2"><ChevronLeft/>Cancel</h2>
+    <div className="p-8">
+      <Link href={"/dashboard"}><h2 className="flex gap-2"><ChevronLeft/>Cancel</h2></Link>
       <div className="mt-4">
         <h2 className="font-bold text-2xl my-4">
           Create New Event
@@ -26,20 +30,20 @@ function MeetingForm() {
       </div>
       <div className="flex flex-col gap-3 my-4">
         <h2 className="font-bold">Event Name *</h2>
-        <Input placeholder="Name of your meeting event"/>
+        <Input placeholder="Name of your meeting event" onChange={(e)=>setEventName(e.target.value)}/>
 
         <h2 className="font-bold">Duration *</h2>
         
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            <Button className="max-w-40" variant="outline">30 Minutes</Button>
+            <Button className="max-w-40" variant="outline">{duration} Minutes</Button>
           </DropdownMenuTrigger>
           <DropdownMenuContent>
            
-            <DropdownMenuItem>15 Minutes</DropdownMenuItem>
-            <DropdownMenuItem>30 Minutes</DropdownMenuItem>
-            <DropdownMenuItem>45 Minutes</DropdownMenuItem>
-            <DropdownMenuItem>60 Minutes</DropdownMenuItem>
+            <DropdownMenuItem onClick={()=>setDuration(15)}>15 Minutes</DropdownMenuItem>
+            <DropdownMenuItem onClick={()=>setDuration(30)}>30 Minutes</DropdownMenuItem>
+            <DropdownMenuItem onClick={()=>setDuration(45)}>45 Minutes</DropdownMenuItem>
+            <DropdownMenuItem onClick={()=>setDuration(60)}>60 Minutes</DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
 
@@ -52,10 +56,13 @@ function MeetingForm() {
             </div>
           ))}
         </div>
+        {location&&<>
         <h2 className="font-bold">Add URL</h2>
-        <Input placeholder="Add the url"/>
+        <Input placeholder="Add the url" onChange={(e)=>setLocationUrl(e.target.value)}/>
+        </>}
 
       </div>
+      <Button disabled={!(eventName||location||locationUrl||duration)}  className="w-full mt-9">Create</Button>
     </div>
   )
 }
